b3b652ebaef0b6d174bdfde4a792be157e41609e
pmorel <delasalle.sio.morel.p@gmail.com>
2019-09-09 12:29:22 +0200

    test de commit

--- app/src/main/java/com/example/lokacar/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/lokacar/ui/login/LoginActivity.java
@@ -115,6 +115,7 @@ public class LoginActivity extends AppCompatActivity {
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
+                
             }
         });
     }
