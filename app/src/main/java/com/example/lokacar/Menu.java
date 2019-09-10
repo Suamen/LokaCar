1fd27d26dff49041d25051315da87db659eacc33
pmorel <delasalle.sio.morel.p@gmail.com>
2019-09-10 11:46:42 +0200

    Menu

--- app/src/main/java/com/example/lokacar/Menu.java
+++ app/src/main/java/com/example/lokacar/Menu.java
@@ -2,7 +2,9 @@ package com.example.lokacar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void clicListeVoituresDisponibles (View view) {
+//        Intent intent = new Intent (this, ListeVoituresActivity.class);
+//        startActivity(intent);
+    }
+
+    public void clicListeVoituresLouees (View view) {
+//        Intent intent = new Intent (this, ListeClientsActivity.class);
+//        startActivity(intent);
+    }
+
+    public void clicChiffreAffaire (View view) {
+//        Intent intent = new Intent (this, ChiffreAffaireActivity.class);
+//        startActivity(intent);
+    }
+
+    public void clicRestitutionVoiture (View view) {
+//        Intent intent = new Intent (this, ListeLocationsActivity.class);
+//        startActivity(intent);
     }
 }
