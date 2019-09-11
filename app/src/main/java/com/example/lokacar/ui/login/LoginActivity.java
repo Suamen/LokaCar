6bc9cccd19d31c23dd142637b072b8adec752542
pmorel <delasalle.sio.morel.p@gmail.com>
2019-09-11 16:38:48 +0200

    Update

--- app/src/main/java/com/example/lokacar/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/lokacar/ui/login/LoginActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -26,12 +27,26 @@ import android.widget.Toast;
 import com.example.lokacar.Menu;
 import com.example.lokacar.R;
 import com.example.lokacar.data.bo.Agence;
+import com.example.lokacar.data.bo.Gerant;
+import com.example.lokacar.data.bo.Location;
+import com.example.lokacar.data.bo.Vehicule;
+import com.example.lokacar.data.dal.AgenceDao;
+import com.example.lokacar.data.dal.GerantDao;
+import com.example.lokacar.data.dal.VehiculeDao;
 import com.example.lokacar.ui.login.LoginViewModel;
 import com.example.lokacar.ui.login.LoginViewModelFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    private GerantDao gerantDao;
+    private AgenceDao agenceDao;
+    private VehiculeDao vehiculeDao;
+    private List<Gerant> listeGerant;
+    private TextView loginError;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
+
+        // Init dao
+        gerantDao = new GerantDao(LoginActivity.this);
+        vehiculeDao = new VehiculeDao(LoginActivity.this);
+        loginError = (TextView) findViewById(R.id.textViewError);
+
+        // Init des données
+        initData();
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
@@ -115,11 +138,31 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+//                loadingProgressBar.setVisibility(View.VISIBLE);
+//                loginViewModel.login(usernameEditText.getText().toString(),
+//                        passwordEditText.getText().toString());
+//                Intent intent = new Intent (LoginActivity.this, Menu.class);
+//                startActivity(intent);
+
                 Intent intent = new Intent (LoginActivity.this, Menu.class);
-                startActivity(intent);
+                listeGerant = new ArrayList<>();
+                listeGerant = gerantDao.selectAll();
+                for (Gerant gerant:listeGerant) {
+                    if ( gerant.getLogin().toString().equalsIgnoreCase(usernameEditText.getText().toString())
+                            &&
+                            gerant.getMdp().toString().equalsIgnoreCase(passwordEditText.getText().toString()) )
+                    {
+                        Log.i("GERANT", gerant.toString());
+                        agenceDao = new AgenceDao(LoginActivity.this);
+                        Agence agence = agenceDao.selectById(gerant.getId()) ;
+                        intent.putExtra("nomAgence", agence.getNomAgence());
+                        intent.putExtra("gerant", gerant.getNom() + " " + gerant.getPrenom());
+                        startActivity(intent);
+                        break;
+                    }else {
+                        loginError.setVisibility(View.VISIBLE);
+                    }
+                }
             }
         });
     }
@@ -132,5 +175,28 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+
+    public void initData() {
+
+        // 1ère insertion dans la base Gérant pour pouvoir s'identifier :
+        Gerant gerant = new Gerant("MOREL", "Patrick", null,
+                "0635259481", "patrick.morel2017@campus-eni.fr", "morel", "mdptest");
+        gerantDao.insertGerant(gerant);
+        gerantDao.insertGerant(new Gerant("Doe", "John", null, "0652859574",
+                "john.doe@gmail.com", "admin", "admin"));
+
+        // 1ère insertion dans la base Agence :
+        Agence agence = new Agence("RENNES", null, new Gerant(1));
+        agenceDao = new AgenceDao(LoginActivity.this);
+        agenceDao.insertAgence(agence);
+
+        // Insert VOITURE
+//        vehiculeDao.insert(new Vehicule("AA-123-CD", "Audi", null, 80000,
+//                false, true, null, new Agence(1, null, null, null)));
+//        vehiculeDao.insert(new Vehicule("AB-456-EF", "Peugeot", null, 100000,
+//                false, true, null, new Agence(1, null, null, null)));
+//        vehiculeDao.insert(new Vehicule("AC-789-GH", "Citroën", null, 210000,
+//                false, true, null, new Agence(1, null, null, null)));
     }
 }
